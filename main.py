fb4f9d33e72c65ae964db0355c90e70fa9e1dbf9
Doswind <ligone2008@gmail.com>
2024-07-26 23:09:35 +0800

    修改：      main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import replicate
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-import app.codeformer as cf
-import app.gfpgan as gf
 
 app = FastAPI()
 
@@ -11,21 +10,27 @@ async def welcome():
 
 @app.post("/predictions/")
 async def restore(request: Request):
+    # 获取请求头中的 Authorization 和 Content-Type
+    authorization = request.headers.get("Authorization")
+    content_type = request.headers.get("Content-Type")
+
     # 解析请求体中的JSON数据
     json_data = await request.json()
 
+    version = json_data.get("version")
+    input = json_data.get("input")
+
     # 调用CodeFormer进行处理
     try:
-        output = gf.run(json_data)
+        client =replicate.Client(api_token=authorization)
+        output = client.run(version, input)
     except Exception as e:
         print(f"Error during CodeFormer processing: {e}")
         return JSONResponse(content={"error": str(e)}, status_code=500)
-
-    # 打印输出内容（可选）
-    print(f"Output: {output}")
 
     return JSONResponse(content={"output": output})
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
